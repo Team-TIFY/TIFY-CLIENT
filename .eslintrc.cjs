cd68731e997835708fc09dbb784d237350fbbcef
Eugene Kim <eugene02876@gmail.com>
2023-06-21 23:23:21 +0900

    [chore]: eslint 및 prettier 세팅 완료

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,10 +14,18 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-   
+    'react/react-in-jsx-scope': "off",
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/no-unknown-property':['error', {'ignore':['css']}],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { 'ignoreRestSiblings' : true}
+    ]
   },
   settings: {
     'import/external-module-folders' : ['.yarn'],
