a77151fa9e4ae14d8c7ea7c68f7d9b8aeaa3cc1d
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-13 12:20:33 +0900

    refactor(ui): Space 컴포넌트 수정

--- src/components/atoms/Spacing/index.tsx
+++ src/components/atoms/Spacing/index.tsx
@@ -1,26 +1,28 @@
-/**  @jsxImportSource @emotion/react */
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { HTMLAttributes, ReactNode } from 'react';
 
 export interface SpacingProps {
-  width: 16 | 24;
-  height: 4 | 8 | 12 | 16 | 20 | 24 | 32 | 48 | 64;
-  isScroll?: boolean;
-  side: boolean;
+  width?: 16 | 24;
+  height?: 4 | 8 | 12 | 16 | 20 | 24 | 32 | 48 | 64;
+  variant?: "default" | "scroll" | "side";
 }
 
 export const Spacing = ({
-  width,
-  height,
-  isScroll = false,
-  side
+  width = 16,
+  height = 24,
+  variant = "default",
+  ...props
 }: SpacingProps) => {
   return (
     <div
       css={css`
-        height: ${side} ? 100% : ${isScroll} ? 48px : ${height}px;
-        width: ${side} ? ${width}px : 100%;
+        width: ${
+          variant === "default" ? "100%" : variant === "side" ? `${width}px` : "100%"
+        };
+        height: ${
+          variant === "default" ? `${height}px` : variant === "scroll" ? "48px" : "100%"
+        };
       `}
     ></div>
-  )
-}
+  );
+};
