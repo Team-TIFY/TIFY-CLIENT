3fde36f8bb636eaabce60e1be8bb426729362f2b
홍서현 <81316050+ghdtjgus76@users.noreply.github.com>
2023-08-23 13:10:20 +0900

    chore(ui): Avatar 컴포넌트 수정 (#64)

--- src/components/atoms/Avatar/Avatar.stories.ts
+++ src/components/atoms/Avatar/Avatar.stories.ts
@@ -10,29 +10,98 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Xsmall: Story = {
+export const Pink1: Story = {
   args: {
     variant: "xsmall",
-    color: "purple",
-    imageUrl: "kitty",
+    imageUrl: "pink1",
     isVisible: "visible",
   }
 };
 
-export const Small: Story = {
+export const Pink2: Story = {
   args: {
-    variant: "small",
-    color: "purple",
-    imageUrl: "kitty",
-    isVisible:"visible",
+    variant: "xsmall",
+    imageUrl: "pink2",
+    isVisible: "visible",
   }
 };
 
-export const Medium: Story = {
+export const Pink3: Story = {
   args: {
-    variant: "medium",
-    color: "purple",
-    imageUrl: "kitty",
+    variant: "xsmall",
+    imageUrl: "pink3",
+    isVisible: "visible",
+  }
+};
+
+export const Pink4: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "pink4",
+    isVisible: "visible",
+  }
+};
+
+export const Transparent1: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "transparent1",
+    isVisible: "visible",
+  }
+};
+
+export const Transparent2: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "transparent2",
+    isVisible: "visible",
+  }
+};
+
+export const Transparent3: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "transparent3",
+    isVisible: "visible",
+  }
+};
+
+export const Transparent4: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "transparent4",
+    isVisible: "visible",
+  }
+};
+
+export const White1: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "white1",
+    isVisible: "visible",
+  }
+};
+
+export const White2: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "white2",
+    isVisible: "visible",
+  }
+};
+
+export const White3: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "white3",
+    isVisible: "visible",
+  }
+};
+
+export const White4: Story = {
+  args: {
+    variant: "xsmall",
+    imageUrl: "white4",
     isVisible: "visible",
   }
 };
