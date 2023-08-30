63c5541565a31d7cea5f8cd7252a64cec6f9336a
Eugene Kim <eugene02876@gmail.com>
2023-08-30 14:41:21 +0900

    fix: 도메인 변경

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://tifyfriends.com/api'
+export const BASE_URL = 'https://tifyfriends.com/api'
 
 export const axiosApi = axios.create({
     baseURL: BASE_URL,
