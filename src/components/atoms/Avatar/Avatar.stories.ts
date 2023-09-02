c84304555ef6fecc07d399a9893afacac84cf189
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-02 15:50:06 +0900

    Chore(ui): Avatar 컴포넌트 랜덤 기능 추가

--- src/components/atoms/Avatar/Avatar.stories.ts
+++ src/components/atoms/Avatar/Avatar.stories.ts
@@ -10,98 +10,16 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Pink1: Story = {
+export const VisibleAvatar: Story = {
   args: {
     variant: "xsmall",
-    imageUrl: "pink1",
     isVisible: "visible",
   }
-};
+}
 
-export const Pink2: Story = {
+export const InvisibleAvatar: Story = {
   args: {
     variant: "xsmall",
-    imageUrl: "pink2",
-    isVisible: "visible",
+    isVisible: "invisible",
   }
-};
-
-export const Pink3: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "pink3",
-    isVisible: "visible",
-  }
-};
-
-export const Pink4: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "pink4",
-    isVisible: "visible",
-  }
-};
-
-export const Purple1: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "purple1",
-    isVisible: "visible",
-  }
-};
-
-export const Purple2: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "purple2",
-    isVisible: "visible",
-  }
-};
-
-export const Purple3: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "purple3",
-    isVisible: "visible",
-  }
-};
-
-export const Purple4: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "purple4",
-    isVisible: "visible",
-  }
-};
-
-export const White1: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "white1",
-    isVisible: "visible",
-  }
-};
-
-export const White2: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "white2",
-    isVisible: "visible",
-  }
-};
-
-export const White3: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "white3",
-    isVisible: "visible",
-  }
-};
-
-export const White4: Story = {
-  args: {
-    variant: "xsmall",
-    imageUrl: "white4",
-    isVisible: "visible",
-  }
-};
+}
