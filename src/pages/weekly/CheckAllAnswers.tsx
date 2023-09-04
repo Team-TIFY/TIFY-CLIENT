824db59a715594356f545f4efd0feae585da147b
Eugene Kim <eugene02876@gmail.com>
2023-09-05 01:13:56 +0900

    feat: 콕 찌르기 버튼 생성

--- src/pages/weekly/CheckAllAnswers.tsx
+++ src/pages/weekly/CheckAllAnswers.tsx
@@ -6,7 +6,9 @@ import { WeeklyApi } from "@utils/apis/weekly/WeeklyApi"
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil";
 import { questionState } from "@libs/store/question";
-
+import { RoundButton } from "@components/atoms/RoundButton";
+import Poke from "@assets/icons/Poke";
+import { FlexBox } from "@components/layouts/FlexBox";
 const CheckAllAnswers = () => {
     const [count, setCount] = useState<number>(0)
     const [question, setQuestion] = useRecoilState(questionState)
@@ -30,11 +32,20 @@ const CheckAllAnswers = () => {
             <DailyQuestionBox />
             <div
                 style={{
-                    background: 'rgb(255, 153, 207, 0.3)', cursor: 'pointer',
+                    background: 'rgb(255, 153, 207, 0.3)', cursor: 'pointer', width: '100%',
                     height: '242px', margin: '60px 0px', color: 'white', textAlign: 'center'
                 }}>
                 이미지 영역
             </div>
+            <RoundButton variant="smallRound" width={240} fullWidth={false}>
+                <FlexBox align={'center'} gap={8}>
+                    답변 안한 친구 찌르러 가기
+                    <Poke />
+                </FlexBox>
+            </RoundButton>
+            <AnswerListContainer>
+
+            </AnswerListContainer>
         </WeekAnswersContainer>
     )
 }
@@ -45,6 +56,11 @@ const WeekAnswersContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
+    align-items: center;
     height: calc(100vh - 80px);
     position: relative;
+`
+
+const AnswerListContainer = styled.div`
+    
 `
