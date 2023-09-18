114ded4f1d192659d0cd223fa4df2025d183a200
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-18 23:43:10 +0900

    feat(ui): FriendsList_C 제작 및 스토리북 작성

---
+++ src/components/atoms/FriendsList/FriendsListD/index.tsx
@@ -0,0 +1,68 @@
+import { Avatar } from '@components/atoms/Avatar'
+import SquareButton from '@components/atoms/SquareButton'
+import { Text } from '@components/atoms/Text'
+import { FlexBox } from '@components/layouts/FlexBox'
+import styled from '@emotion/styled'
+
+interface FriendsListDProps {
+  nickName: string
+  friendsNumber: number
+  onAcceptButtonClick?: () => void
+  onDeleteButtonClick?: () => void
+}
+
+const FriendsListD = ({
+  nickName,
+  friendsNumber,
+  onAcceptButtonClick,
+  onDeleteButtonClick,
+}: FriendsListDProps) => {
+  return (
+    <Wrapper>
+      <ProfileWrapper>
+        <Avatar variant="small" />
+        <InfoWrapper>
+          <Text typo="Subhead_14" color="white">
+            @{nickName.length > 12 ? nickName.slice(0, 11) + '···' : nickName}
+          </Text>
+          <Text typo="Caption_10" color="gray_400">
+            함께 아는 친구 {friendsNumber}명
+          </Text>
+        </InfoWrapper>
+      </ProfileWrapper>
+      <ButtonWrapper>
+        <SquareButton variant={'xsmallSquareP'} onClick={onAcceptButtonClick}>
+          수락
+        </SquareButton>
+        <SquareButton variant={'xsmallSquareS'} onClick={onDeleteButtonClick}>
+          삭제
+        </SquareButton>
+      </ButtonWrapper>
+    </Wrapper>
+  )
+}
+
+export default FriendsListD
+
+const Wrapper = styled(FlexBox)`
+  width: 328px;
+  height: 48px;
+  justify-content: space-between;
+`
+
+const ProfileWrapper = styled(FlexBox)`
+  width: 180px;
+  height: 100%;
+  gap: 12px;
+  justify-content: flex-start;
+`
+
+const InfoWrapper = styled(FlexBox)`
+  flex-direction: column;
+  gap: 2px;
+`
+
+const ButtonWrapper = styled(FlexBox)`
+  height: 100%;
+  gap: 4px;
+`
