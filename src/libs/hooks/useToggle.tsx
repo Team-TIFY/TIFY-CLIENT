416ede4eb0b4d1ca2654545eb41e46f1a2636a56
ghdtjgus76 <ghdtjgus76@naver.com>
2023-10-05 15:24:00 +0900

    chore: useToggle 타입 정의 변경 (함수 반환값 지정)

--- src/libs/hooks/useToggle.tsx
+++ src/libs/hooks/useToggle.tsx
@@ -1,13 +1,15 @@
 import { useCallback, useState } from 'react'
 
-const useToggle = <T extends boolean>(initialState = true) => {
+type useToggleReturnType = readonly [boolean, () => void]
+
+const useToggle = (initialState = true): useToggleReturnType => {
   const [state, setState] = useState(initialState)
 
   const toggle = useCallback(() => {
     setState((prevState) => !prevState)
   }, [])
 
-  return [state, toggle] as [T, () => void]
+  return [state, toggle]
 }
 
 export default useToggle
