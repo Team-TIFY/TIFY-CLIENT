07d4ed6523fb055642ac71e18e5b177685e64e0d
ghdtjgus76 <ghdtjgus76@naver.com>
2023-10-20 16:13:22 +0900

    fix: 친구 요청 수락, 삭제 invalidateQueries 속성 추가

--- src/libs/hooks/useFriendMutate.ts
+++ src/libs/hooks/useFriendMutate.ts
@@ -1,13 +1,21 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { FriendsApi } from '@utils/apis/friends/FriendsApi'
 
 const useFriendMutate = () => {
+  const queryClient = useQueryClient()
+
   const { mutate: acceptFriendRequestMutate } = useMutation(
     FriendsApi.ACCEPT_FRIEND_REQUEST,
+    {
+      onSuccess: () => queryClient.invalidateQueries(['friendRequestList']),
+    },
   )
 
   const { mutate: rejectFriendRequestMutate } = useMutation(
     FriendsApi.REJECT_FRIEND_REQUEST,
+    {
+      onSuccess: () => queryClient.invalidateQueries(['friendRequestList']),
+    },
   )
 
   return { acceptFriendRequestMutate, rejectFriendRequestMutate }
