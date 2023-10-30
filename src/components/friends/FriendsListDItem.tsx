6a73e76be97d46fbb6d60103c4e8f1625ef6ba18
ghdtjgus76 <ghdtjgus76@naver.com>
2023-10-30 11:34:11 +0900

    chore: 친구 요청 목록 구현 (모든 요청 보기 버튼 동작)

--- src/components/friends/FriendsListDItem.tsx
+++ src/components/friends/FriendsListDItem.tsx
@@ -6,69 +6,62 @@ import { FlexBox } from '@components/layouts/FlexBox'
 import styled from '@emotion/styled'
 import useFriendMutate from '@libs/hooks/useFriendMutate'
 import { FriendRequestType } from '@utils/apis/friends/FriendsType'
+import { useCallback, useState } from 'react'
 
 export type FriendsListDItemProps = {
   friendsList: FriendRequestType[]
 }
 
 const FriendsListDItem = ({ friendsList }: FriendsListDItemProps) => {
+  const [isAllRequest, setIsAllRequest] = useState(false)
+
   const { acceptFriendRequestMutate, rejectFriendRequestMutate } =
     useFriendMutate()
 
-  const handleAcceptButtonClick = (acceptRequestId: number) => {
+  const handleClickAcceptButton = (acceptRequestId: number) => {
     acceptFriendRequestMutate(acceptRequestId)
   }
 
-  const handleDeleteButtonClick = (deleteRequestId: number) => {
+  const handleClickDeleteButton = (deleteRequestId: number) => {
     rejectFriendRequestMutate(deleteRequestId)
   }
 
+  const getFriendsList = (
+    friendsList: FriendRequestType[],
+    isAllRequest: boolean,
+  ) => {
+    return isAllRequest ? friendsList : friendsList.slice(0, 4)
+  }
+
+  const renderFriendsList = useCallback(() => {
+    return getFriendsList(friendsList, isAllRequest).map((requestFriend) => (
+      <FriendsListD
+        key={requestFriend.neighborApplicationId}
+        nickName={requestFriend.toUserInfo.userName}
+        friendsNumber={requestFriend.mutualNeighborCounts}
+        onAcceptButtonClick={() =>
+          handleClickAcceptButton(requestFriend.neighborApplicationId)
+        }
+        onDeleteButtonClick={() =>
+          handleClickDeleteButton(requestFriend.neighborApplicationId)
+        }
+      />
+    ))
+  }, [friendsList, isAllRequest])
+
   return (
     <FriendsListWrapper>
-      {friendsList.length > 4 ? (
-        <>
-          {friendsList.slice(0, 4).map((requestFriend) => (
-            <FriendsListD
-              key={requestFriend.neighborApplicationId}
-              nickName={requestFriend.toUserInfo.userName}
-              friendsNumber={requestFriend.mutualNeighborCounts}
-              onAcceptButtonClick={() =>
-                handleAcceptButtonClick(requestFriend.neighborApplicationId)
-              }
-              onDeleteButtonClick={() =>
-                handleDeleteButtonClick(requestFriend.neighborApplicationId)
-              }
-            />
-          ))}
-          <FlexBox
-            gap={4}
-            style={{
-              padding: '16px',
-              cursor: 'pointer',
-            }}
-          >
-            <Text
-              typo="Caption_12R"
-              children="모든 요청 보기"
-              color="gray_400"
-            />
-            <Svg children={<Plus />} />
-          </FlexBox>
-        </>
-      ) : (
-        friendsList.map((requestFriend) => (
-          <FriendsListD
-            key={requestFriend.neighborApplicationId}
-            nickName={requestFriend.toUserInfo.userName}
-            friendsNumber={requestFriend.mutualNeighborCounts}
-            onAcceptButtonClick={() =>
-              handleAcceptButtonClick(requestFriend.neighborApplicationId)
-            }
-            onDeleteButtonClick={() =>
-              handleDeleteButtonClick(requestFriend.neighborApplicationId)
-            }
+      <FlexBox direction="column" gap={16}>
+        {renderFriendsList()}
+      </FlexBox>
+      {!isAllRequest && friendsList.length && (
+        <AllRequestWrapper onClick={() => setIsAllRequest(true)}>
+          <Text typo="Caption_12R" children="모든 요청 보기" color="gray_400" />
+          <Svg
+            children={<Plus />}
+            style={{ display: 'flex', alignItems: 'center' }}
           />
-        ))
+        </AllRequestWrapper>
       )}
     </FriendsListWrapper>
   )
@@ -77,6 +70,13 @@ const FriendsListDItem = ({ friendsList }: FriendsListDItemProps) => {
 export default FriendsListDItem
 
 const FriendsListWrapper = styled(FlexBox)`
-  flex-wrap: wrap;
-  gap: 16px;
+  flex-direction: column;
+`
+
+const AllRequestWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 16px;
+  cursor: pointer;
 `
