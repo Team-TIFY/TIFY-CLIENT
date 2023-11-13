9fa9c09b9c7193638a8114c6630cc0fc5e547d75
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-13 22:27:33 +0900

    chore: 프로필 배경 이미지 추가

--- src/components/profile/ProfileImage.tsx
+++ src/components/profile/ProfileImage.tsx
@@ -1,11 +1,47 @@
 import styled from '@emotion/styled'
+import backgroundImage from '@assets/image/profile_background.png'
 
 export const ProfileImage = () => {
-  return <ImageWrapper></ImageWrapper>
+  return (
+    <ImageWrapper>
+      <BackgroundImage src={backgroundImage} />
+    </ImageWrapper>
+  )
 }
 
 const ImageWrapper = styled.div`
-  border: 1px solid red;
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @keyframes moveToY1 {
+    from {
+      transform: translateY(0px);
+    }
+    to {
+      transform: translateY(-20px);
+    }
+  }
+  @keyframes moveToY2 {
+    from {
+      transform: translateY(0px);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
   width: 100%;
-  height: 336px;
+  height: auto;
+  position: relative;
+`
+
+const BackgroundImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
 `
