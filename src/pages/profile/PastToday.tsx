979edf8f673406005852ab93a5b730b4e9330aec
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-01 19:11:04 +0900

    fix: 지난 투데이 카테고리 개수 답변 api 호출 시 잘못된 인자 전달 문제 해결

--- src/pages/profile/PastToday.tsx
+++ src/pages/profile/PastToday.tsx
@@ -2,18 +2,15 @@ import { Spacing } from '@components/atoms/Spacing'
 import TodayCategory from '@components/atoms/TodayCategory'
 import { categoryList } from '@components/atoms/TodayCategoryList'
 import { FlexBox } from '@components/layouts/FlexBox'
-import { authState } from '@libs/store/auth'
 import { useQuery } from '@tanstack/react-query'
 import { UserApi } from '@utils/apis/user/UserApi'
 import { useParams } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
 
 const PastToday = () => {
-  const auth = useRecoilValue(authState)
   const location = useParams()
 
   const { data } = useQuery(['pastTodayCategoryCount'], () =>
-    UserApi.GET_PAST_TODAY_CATEGORY_COUNT(auth.userProfile.id),
+    UserApi.GET_PAST_TODAY_CATEGORY_COUNT(Number(location.id)),
   )
 
   return (
