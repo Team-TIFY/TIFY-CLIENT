18d1658871cb4547b82a6476671dcd2a7cc410ea
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-17 00:21:25 +0900

    feat: 취향 상자 이미지 추가

--- src/utils/tasteBoxImage.ts
+++ src/utils/tasteBoxImage.ts
@@ -2,16 +2,16 @@ import { TasteBoxVariantType } from './apis/favor/TasteType'
 
 export const tasteBoxImage: Record<TasteBoxVariantType, string> = {
   LIP: '/images/makeup.png',
-  EYE: '/images/makeup.png',
-  PERFUME: '/images/makeup.png',
-  MOISTURE: '/images/makeup.png',
-  PLACE: '/images/fragrance.png',
-  TOP: '/images/fragrance.png',
-  FAS_PRODUCT: '/images/makeup.png',
-  DIG_PRODUCT: '/images/makeup.png',
-  BAG: '/images/makeup.png',
-  ACCESSORY: '/images/keyring.png',
-  DISH: '/images/makeup.png',
-  CUP: '/images/cooking.png',
+  EYE: '/images/eyeshadow.png',
+  PERFUME: '/images/fragrance.png',
+  MOISTURE: '/images/makeup.png', // ?
+  PLACE: '/images/fragrance.png', // ?
+  TOP: '/images/clothes.png',
+  FAS_PRODUCT: '/images/makeup.png', // ?
+  DIG_PRODUCT: '/images/makeup.png', // ?
+  BAG: '/images/bag.png',
+  ACCESSORY: '/images/accessary.png',
+  DISH: '/images/makeup.png', // ?
+  CUP: '/images/cup.png',
   EXERCISE: '/images/exercise.png',
 }
