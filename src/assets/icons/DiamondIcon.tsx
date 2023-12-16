5ac988e1a0ffb04ee50758d58c139f1b022391fb
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-16 18:58:44 +0900

    feat: 태그 아이콘 추가 및 질문에 따른 아이콘 로직 작성

---
+++ src/assets/icons/DiamondIcon.tsx
@@ -0,0 +1,37 @@
+const DiamondIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon-tag-16" clipPath="url(#clip0_4856_9953)">
+        <path
+          id="Vector"
+          d="M4 3.3335H12L14 6.66683L8.33333 13.0002C8.28988 13.0445 8.23802 13.0797 8.18078 13.1038C8.12355 13.1278 8.06209 13.1402 8 13.1402C7.93792 13.1402 7.87645 13.1278 7.81922 13.1038C7.76198 13.0797 7.71012 13.0445 7.66667 13.0002L2 6.66683L4 3.3335Z"
+          stroke="#7D40FF"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M6.66536 8.00003L5.33203 6.53337L5.73203 5.8667"
+          stroke="#7D40FF"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4856_9953">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default DiamondIcon
---
+++ src/assets/icons/SizeIcon.tsx
@@ -0,0 +1,61 @@
+const SizeIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon-tag-16" clipPath="url(#clip0_4856_10324)">
+        <path
+          id="Vector"
+          d="M11.3333 2L14 4.66667L4.66667 14L2 11.3333L11.3333 2Z"
+          stroke="#F74AA6"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M10.666 4.6665L9.66602 3.6665"
+          stroke="#F74AA6"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_3"
+          d="M8.66602 6.6665L7.66602 5.6665"
+          stroke="#F74AA6"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_4"
+          d="M6.66602 8.6665L5.66602 7.6665"
+          stroke="#F74AA6"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_5"
+          d="M4.66602 10.6665L3.66602 9.6665"
+          stroke="#F74AA6"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4856_10324">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default SizeIcon
