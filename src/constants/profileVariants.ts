218dfb179b901060f632726bef8606e461152c87
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-29 01:58:49 +0900

    refactor: Avatar 컴포넌트 리팩토링

---
+++ src/constants/profileVariants.ts
@@ -0,0 +1,27 @@
+import Pink1 from '/images/pink1.png'
+import Pink2 from '/images/pink2.png'
+import Pink3 from '/images/pink3.png'
+import Pink4 from '/images/pink4.png'
+import Purple1 from '/images/purple1.png'
+import Purple2 from '/images/purple2.png'
+import Purple3 from '/images/purple3.png'
+import Purple4 from '/images/purple4.png'
+import White1 from '/images/white1.png'
+import White2 from '/images/white2.png'
+import White3 from '/images/white3.png'
+import White4 from '/images/white4.png'
+
+export const profileVariants = [
+  Pink1,
+  Pink2,
+  Pink3,
+  Pink4,
+  Purple1,
+  Purple2,
+  Purple3,
+  Purple4,
+  White1,
+  White2,
+  White3,
+  White4,
+]
