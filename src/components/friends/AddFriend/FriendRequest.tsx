0bf6e62385295001a29cfd9135cb1069961889b9
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 01:03:45 +0900

    refactor: FriendRequest 컴포넌트 리팩토링

--- src/components/friends/AddFriend/FriendRequest.tsx
+++ src/components/friends/AddFriend/FriendRequest.tsx
@@ -1,19 +1,21 @@
-import { useQuery } from '@tanstack/react-query'
 import { useRecoilValue } from 'recoil'
+import { useQuery } from '@tanstack/react-query'
+
+import { authState } from '@libs/store/auth'
+import { friendState } from '@libs/store/friend'
+import { FriendsApi } from '@utils/apis/friends/FriendsApi'
 import { FlexBox } from '@components/layouts/FlexBox'
 import { Text } from '@components/atoms/Text'
-import FriendsListDItem from '../FriendsList/FriendsListDItem'
-import { authState } from '@libs/store/auth'
-import { FriendsApi } from '@utils/apis/friends/FriendsApi'
 import { Spacing } from '@components/atoms/Spacing'
-import { friendState } from '@libs/store/friend'
+import FriendsListDItem from '../FriendsList/FriendsListDItem'
+import { friendsQueryKeys } from '@constants/queryKeys/friendsQueryKeys'
 
 const FriendRequest = () => {
   const auth = useRecoilValue(authState)
   const friendStateData = useRecoilValue(friendState)
 
   const { data: friendRequestList = [] } = useQuery(
-    ['friendRequestList', auth.userProfile.id],
+    [friendsQueryKeys.FRIEND_REQUEST_LIST, auth.userProfile.id],
     FriendsApi.GET_FRIEND_REQUEST_LIST,
   )
 
@@ -21,20 +23,22 @@ const FriendRequest = () => {
     return friendStateData.showPartialRequest ? 24 : 64
   }
 
-  return friendRequestList.length ? (
-    <FlexBox direction="column">
-      <Text
-        typo="Caption_12R"
-        children="친구 요청"
-        color="gray_100"
-        style={{ padding: '16px 16px 8px 16px', width: '328px' }}
-      />
+  return (
+    friendRequestList.length && (
       <FlexBox direction="column">
-        <FriendsListDItem friendsList={friendRequestList} />
+        <Text
+          typo="Caption_12R"
+          children="친구 요청"
+          color="gray_100"
+          style={{ padding: '16px 16px 8px 16px', width: '328px' }}
+        />
+        <FlexBox direction="column">
+          <FriendsListDItem friendsList={friendRequestList} />
+        </FlexBox>
+        <Spacing height={getSpacingHeight()} />
       </FlexBox>
-      <Spacing height={getSpacingHeight()} />
-    </FlexBox>
-  ) : null
+    )
+  )
 }
 
 export default FriendRequest
--- src/constants/queryKeys/friendsQueryKeys.ts
+++ src/constants/queryKeys/friendsQueryKeys.ts
@@ -1,4 +1,5 @@
 export const enum friendsQueryKeys {
   FRIENDS_LIST = 'friendsList',
   SEARCH_FRIEND = 'searchFriend',
+  FRIEND_REQUEST_LIST = 'friendRequestList',
 }
