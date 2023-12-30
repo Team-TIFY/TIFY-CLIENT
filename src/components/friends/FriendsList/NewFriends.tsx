cd7308b39ed1f8f5f567a8f19a254ecbdda877ee
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 01:55:17 +0900

    refactor: NewFriends 컴포넌트 리팩토링

--- src/components/friends/FriendsList/NewFriends.tsx
+++ src/components/friends/FriendsList/NewFriends.tsx
@@ -1,55 +1,70 @@
-import { FlexBox } from '@components/layouts/FlexBox'
-import { Text } from '@components/atoms/Text'
-import Option from '@components/atoms/Option'
-import styled from '@emotion/styled'
-import FriendsListBItem from './FriendsListBItem'
-import { Spacing } from '@components/atoms/Spacing'
-import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
+import { useQuery } from '@tanstack/react-query'
+import styled from '@emotion/styled'
+
 import { authState } from '@libs/store/auth'
 import { FriendsApi } from '@utils/apis/friends/FriendsApi'
-import { useEffect } from 'react'
+import { friendsQueryKeys } from '@constants/queryKeys/friendsQueryKeys'
+import { NewFriendsType } from '@models/apis/friends/FriendsType'
+import { FlexBox } from '@components/layouts/FlexBox'
+import { Text } from '@components/atoms/Text'
+import { Spacing } from '@components/atoms/Spacing'
+import Option from '@components/atoms/Option'
+import FriendsListBItem from './FriendsListBItem'
 
 const NewFriends = () => {
   const auth = useRecoilValue(authState)
 
+  const [sortedNewFriendsList, setSortedNewFriendsList] = useState<
+    NewFriendsType[]
+  >([])
+
   const { data: newFriendsList = [] } = useQuery(
-    ['newFriendsList', auth.userProfile.id],
+    [friendsQueryKeys.NEW_FRIENDS_LIST, auth.userProfile.id],
     FriendsApi.GET_NEW_FRIENDS_LIST,
   )
 
   useEffect(() => {
-    newFriendsList.sort((a, b) => a.neighborName.localeCompare(b.neighborName))
+    const sortedNewFriendsListData = [...newFriendsList].sort((a, b) =>
+      a.neighborName.localeCompare(b.neighborName),
+    )
+    setSortedNewFriendsList(sortedNewFriendsListData)
   }, [newFriendsList])
 
-  return newFriendsList.length ? (
-    <>
-      <FlexBox justify="flex-start" style={{ width: '100%', padding: '16px' }}>
-        <Text
-          typo="Caption_12R"
-          children="새로운 친구"
-          color="gray_100"
-          style={{
-            margin: '0 4px 0 0',
-          }}
-        />
-        <Text
-          typo="Mont_Caption_12M"
-          children={newFriendsList.length}
-          color="gray_400"
-          style={{ marginRight: '8px' }}
-        />
-        <Option variant="new" />
-      </FlexBox>
-      <FriendsListBItemWrapper>
-        <FriendsListBItem
-          friendsList={newFriendsList}
-          isNewFriendsList={true}
-        />
-      </FriendsListBItemWrapper>
-      <Spacing height={16} />
-    </>
-  ) : null
+  return (
+    sortedNewFriendsList.length && (
+      <>
+        <FlexBox
+          justify="flex-start"
+          style={{ width: '100%', padding: '16px' }}
+        >
+          <Text
+            typo="Caption_12R"
+            children="새로운 친구"
+            color="gray_100"
+            style={{
+              margin: '0 4px 0 0',
+            }}
+          />
+          <Text
+            typo="Mont_Caption_12M"
+            children={sortedNewFriendsList.length}
+            color="gray_400"
+            style={{ marginRight: '8px' }}
+          />
+          <Option variant="new" />
+        </FlexBox>
+        <FriendsListBItemWrapper>
+          <FriendsListBItem
+            friendsList={sortedNewFriendsList}
+            isNewFriendsList={true}
+          />
+        </FriendsListBItemWrapper>
+        <Spacing height={16} />
+      </>
+    )
+  )
 }
 
 export default NewFriends
--- src/constants/queryKeys/friendsQueryKeys.ts
+++ src/constants/queryKeys/friendsQueryKeys.ts
@@ -1,6 +1,7 @@
 export const enum friendsQueryKeys {
   FRIENDS_LIST = 'friendsList',
   BIRTHDAY_FRIENDS_LIST = 'birthdayFriendsList',
+  NEW_FRIENDS_LIST = 'newFriendsList',
   SEARCH_FRIEND = 'searchFriend',
   FRIEND_REQUEST_LIST = 'friendRequestList',
 }
