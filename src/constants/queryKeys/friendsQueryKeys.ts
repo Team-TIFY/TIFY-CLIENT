27c31711ea6535540fdbc2803873a693d9253017
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 01:44:55 +0900

    refactor: BirthdayFriends 컴포넌트 리팩토링

--- src/constants/queryKeys/friendsQueryKeys.ts
+++ src/constants/queryKeys/friendsQueryKeys.ts
@@ -1,5 +1,6 @@
 export const enum friendsQueryKeys {
   FRIENDS_LIST = 'friendsList',
+  BIRTHDAY_FRIENDS_LIST = 'birthdayFriendsList',
   SEARCH_FRIEND = 'searchFriend',
   FRIEND_REQUEST_LIST = 'friendRequestList',
 }
