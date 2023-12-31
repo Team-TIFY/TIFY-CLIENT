2510c08a7ec6e7f6ee3990d6b08d2d8fcee356f8
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 18:31:49 +0900

    refactor: PastToday 페이지 리팩토링

--- src/constants/queryKeys/profileQueryKeys.ts
+++ src/constants/queryKeys/profileQueryKeys.ts
@@ -3,4 +3,5 @@ export const enum profileQueryKeys {
   USER_TAG = 'filteredUserTag',
   NEW_TASTE_ISANSWERED_CATEGORY = 'isAnsweredCategory',
   PAST_TODAY_ANSWER = 'pastTodayAnswer',
+  PAST_TODAY_CATEGORY_COUNT = 'pastTodayCategoryCount'
 }
--- src/pages/profile/PastToday/PastToday.tsx
+++ src/pages/profile/PastToday/PastToday.tsx
@@ -1,15 +1,17 @@
-import { Spacing } from '@components/atoms/Spacing'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+
+import { UserApi } from '@apis/user/UserApi'
 import TodayCategory from '@components/atoms/TodayCategory'
+import { Spacing } from '@components/atoms/Spacing'
 import { categoryList } from '@constants/atoms/todayCategory'
 import { FlexBox } from '@components/layouts/FlexBox'
-import { useQuery } from '@tanstack/react-query'
-import { UserApi } from '@apis/user/UserApi'
-import { useParams } from 'react-router-dom'
+import { profileQueryKeys } from '@constants/queryKeys/profileQueryKeys'
 
 const PastToday = () => {
   const location = useParams()
 
-  const { data } = useQuery(['pastTodayCategoryCount'], () =>
+  const { data } = useQuery([profileQueryKeys.PAST_TODAY_CATEGORY_COUNT], () =>
     UserApi.GET_PAST_TODAY_CATEGORY_COUNT(Number(location.id)),
   )
 
