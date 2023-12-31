ba898c7e63b20d2a35558eb7fb31be7174e4636c
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 18:38:50 +0900

    refactor: EditProfile 페이지 리팩토링

--- src/pages/profile/EditProfile/EditProfile.tsx
+++ src/pages/profile/EditProfile/EditProfile.tsx
@@ -1,26 +1,30 @@
 import styled from '@emotion/styled'
 import { useRecoilValue } from 'recoil'
+
+import { profileState } from '@libs/store/profile'
+import { useOutsideClick } from '@libs/hooks/useOutsideClick'
+import useSetProfileRecoilState from '@libs/hooks/useSetProfileRecoilState'
 import Dimmer from '@components/layouts/Dimmer'
 import { FlexBox } from '@components/layouts/FlexBox'
 import EditProfileImage from '@components/profile/EditProfile/EditProfileImage'
 import ProfileMenuButtons from '@components/profile/ProfileInfo/ProfileMenuButton'
 import EditUserInfo from './EditUserInfo'
-import { profileState } from '@libs/store/profile'
-import { useOutsideClick } from '@libs/hooks/useOutsideClick'
-import useSetProfileRecoilState from '@libs/hooks/useSetProfileRecoilState'
 
 const EditProfile = () => {
   const profileStateData = useRecoilValue(profileState)
   const { setIsEditImageMenuOpen } = useSetProfileRecoilState()
+
   const isEditImageMenuOpen = profileStateData.isEditImageMenuOpen ?? false
 
   const [outsideRef, handleClickEditProfileDimmer] = useOutsideClick(() =>
     setIsEditImageMenuOpen(false),
   )
 
-  const renderIsEditProfileMenuButtons = () => {
-    return (
-      isEditImageMenuOpen && (
+  return (
+    <EditProfileWrapper>
+      <EditProfileImage />
+      <EditUserInfo />
+      {isEditImageMenuOpen && (
         <>
           <Dimmer
             dimmerRef={outsideRef}
@@ -28,15 +32,7 @@ const EditProfile = () => {
           />
           <ProfileMenuButtons type="editProfile" />
         </>
-      )
-    )
-  }
-
-  return (
-    <EditProfileWrapper>
-      <EditProfileImage />
-      <EditUserInfo />
-      {renderIsEditProfileMenuButtons()}
+      )}
     </EditProfileWrapper>
   )
 }
